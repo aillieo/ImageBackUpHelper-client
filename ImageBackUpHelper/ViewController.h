bbe5240637a137560afe99b6e5a3cc21027b4c3d
aillieo <aillieo@163.com>
2017-05-28 21:17:29 +0800

    init project

---
+++ ImageBackUpHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageBackUpHelper
+//
+//  Created by yiyiran on 17/5/28.
+//  Copyright © 2017年 AillieoTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageBackUpHelper/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ImageBackUpHelper
+//
+//  Created by yiyiran on 17/5/28.
+//  Copyright © 2017年 AillieoTech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
